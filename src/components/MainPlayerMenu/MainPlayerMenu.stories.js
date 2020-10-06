632e94b8133fed2288fe7f3975fca466a6b52306
Phil9219 <philipp.fell@yahoo.de>
2020-10-06 16:58:54 +0200

    Add Icons

---
+++ src/components/MainPlayerMenu/MainPlayerMenu.stories.js
@@ -0,0 +1,21 @@
+import "./MainPlayerMenu.css";
+
+import backwardbtn from "./iconBackward.html";
+import forwardbtn from "./iconBackward.html";
+import playpausebtn from "./iconBackward.html";
+import playlistbtn from "./iconPlaylist.html";
+import shufflebtn from "./iconShuffle.html";
+import repeatbtn from "./iconRepeat.html";
+import eqbtn from "./iconEQ.html";
+import favoritebtn from "./iconFavorites.html";
+
+export default { title: "Components/MainPlayerMenu" };
+
+export const BackwardIcon = () => backwardbtn;
+export const ForwardIcon = () => forwardbtn;
+export const PlayPauseIcon = () => playpausebtn;
+export const PlaylistIcon = () => playlistbtn;
+export const ShuffleIcon = () => shufflebtn;
+export const RepeatIcon = () => repeatbtn;
+export const EQIcon = () => eqbtn;
+export const FavoriteIcon = () => favoritebtn;
