601c33a59a430397db000b811b8f937e57ff3d1c
Phil9219 <philipp.fell@yahoo.de>
2020-10-14 10:50:33 +0200

    Add if condition Pausebtn

--- src/components/track/track.js
+++ src/components/track/track.js
@@ -1,6 +1,7 @@
 import "./track.css";
 
 import playActionButton from "../../assets/playActionButton.svg";
+import pauseActionButton from "../../assets/icon-pause-gradient.svg";
 
 export function createTrackElement(track) {
   const divElement = document.createElement("div");
@@ -8,8 +9,10 @@ export function createTrackElement(track) {
 
   const titleElement = document.createElement("h3");
   titleElement.innerText = track.title;
+
   const artistElement = document.createElement("p");
   artistElement.innerText = track.artist;
+
   const imgElement = document.createElement("img");
   imgElement.src = track.imgsrc;
   imgElement.alt = `Image of ${track.artist}`;
@@ -18,6 +21,7 @@ export function createTrackElement(track) {
   const buttonElement = document.createElement("button");
   const playButtonImage = document.createElement("img");
   playButtonImage.src = playActionButton;
+
   buttonElement.className = "track__button";
 
   const descriptionElement = document.createElement("div");
@@ -25,10 +29,20 @@ export function createTrackElement(track) {
   descriptionElement.append(titleElement, artistElement);
   divElement.append(imgElement, descriptionElement, buttonElement);
 
+  let isPlaying = false;
+
   const audioElement = new Audio(track.audio);
   buttonElement.append(playButtonImage);
   buttonElement.onclick = function () {
-    audioElement.play();
+    if (isPlaying === true) {
+      audioElement.pause();
+      playButtonImage.src = playActionButton;
+      isPlaying = false;
+    } else {
+      audioElement.play();
+      playButtonImage.src = pauseActionButton;
+      isPlaying = true;
+    }
   };
 
   return divElement;
